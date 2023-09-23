b8bdd5567ae62cfe9a60f9caeccc157ff7a537fc
kaito121855 <kaito.121855@gmail.com>
2023-09-23 23:15:33 +0900

    add: sampleページコンポーネントの追加

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import styles from './Button.module.css'
+ 
+type ButtonProp = {
+  children: string
+}
+
+export const Button = (props: ButtonProp): JSX.Element => {
+  return (
+    <button type="button" className={ styles.red }>
+      { props.children }
+    </button>
+  )
+}
---
+++ src/pages/sample.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/Button"
+
+const Sample = (): JSX.Element => {
+  return (
+    <div>
+      <Button children="ボタン"></Button>
+    </div>
+  )
+}
+
+export default Sample
