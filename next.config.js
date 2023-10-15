97f1c03b8b2b52ea3c05befe84a7540aa03fa975
kaito121855 <kaito.121855@gmail.com>
2023-10-15 19:42:43 +0900

    fix: next.jsの設定ファイルのオプション変更（警告削除）

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  emotion: true,
+  compiler: {
+    emotion: true,
+  },
 };
 
 module.exports = nextConfig;
