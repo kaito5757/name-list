0a90219835acc98331af1e9f57efa49a3b104a73
kaito121855 <kaito.121855@gmail.com>
2023-12-24 15:32:02 +0900

    change: ナビゲーションにLinkコンポーネントを適用

--- src/components/parts/common/GlobalNavigation/index.tsx
+++ src/components/parts/common/GlobalNavigation/index.tsx
@@ -1,31 +1,45 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import HomeIcon from "@mui/icons-material/Home";
-import TwitterIcon from "@mui/icons-material/Twitter";
+import DesignServicesIcon from "@mui/icons-material/DesignServices";
 import { SxProps, Theme } from "@mui/material";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Box from "@mui/material/Box";
+import Link from "next/link";
 import { memo } from "react";
 
-interface GlobalNavigationProps extends React.ComponentPropsWithoutRef<"div"> {}
-
-type CssTypes = {
+const css: {
   box: SxProps<Theme>;
-};
-
-const css: CssTypes = {
+} = {
   box: {
     marginTop: "3rem",
   },
 };
-export default memo(function GlobalNavigation(props: GlobalNavigationProps) {
+export default function GlobalNavigation() {
   return (
     <Box component="div" sx={css.box}>
       <BottomNavigation showLabels>
-        <BottomNavigationAction label="Home" icon={<HomeIcon />} href="/" />
-        <BottomNavigationAction label="GitHub" icon={<GitHubIcon />} href="/" />
-        <BottomNavigationAction label="X" icon={<TwitterIcon />} href="/" />
+        <BottomNavigationAction
+          LinkComponent={Link}
+          label="Home"
+          icon={<HomeIcon />}
+          href="/"
+        />
+        <BottomNavigationAction
+          LinkComponent={Link}
+          label="GitHub"
+          icon={<GitHubIcon />}
+          href="https://github.com/kaito5757/name-list"
+          target="blank"
+        />
+        <BottomNavigationAction
+          LinkComponent={Link}
+          label="Qiita"
+          icon={<DesignServicesIcon />}
+          href="https://qiita.com/kaito121855"
+          target="blank"
+        />
       </BottomNavigation>
     </Box>
   );
-});
+};
