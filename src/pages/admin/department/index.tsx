cf8ce622de834710b9ddceb495bbf1e370d2dc7a
kaito121855 <kaito.121855@gmail.com>
2023-12-24 11:15:02 +0900

    change: 部署管理の更新時に、ローディングとメッセージが表示されるように変更

--- src/pages/admin/department/index.tsx
+++ src/pages/admin/department/index.tsx
@@ -1,12 +1,15 @@
 import DepartmentCreateDialog from "@/components/pages/Department/DepartmentCreateDialog";
 import DepartmentTable from "@/components/pages/Department/DepartmentTable";
+import { useBasicBackdropStore } from "@/components/parts/BasicBackdrop";
 import BasicButtons from "@/components/parts/BasicButtons";
+import { useBasicSnackbarStore } from "@/components/parts/BasicSnackbar";
 import GlobalLayout from "@/components/parts/common/GlobalLayout";
 import { DepartmentFormSchemaType } from "@/components/schema/departmentFormSchema";
 import { Departments } from "@/types";
 import { trpc } from "@/utils/trpc";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Box, SxProps, Theme } from "@mui/material";
+import { AlertColor } from "@mui/material/Alert";
 import { useEffect, useState } from "react";
 
 const css: {
@@ -28,20 +31,45 @@ const css: {
 };
 
 export default function Department() {
+  const [departments, setDepartments] = useState<Departments>();
+
+  const setBackdrop = useBasicBackdropStore((state) => state.setOpen);
+  const setSnackbar = useBasicSnackbarStore((state) => state.setSnackbar);
+
   const departmentsQuery = trpc.department.getAllDepartments.useQuery();
-  const refetchDepartments = {
-    onSuccess: () => departmentsQuery.refetch(),
+  const setBackdropAndSnackbar = (severity: AlertColor, message: string) => {
+    setBackdrop(false);
+    setSnackbar(severity, message);
   };
-  const createDepartmentMutation =
-    trpc.department.createDepartment.useMutation(refetchDepartments);
+  const refetchDepartments = (severity: AlertColor, message: string) => {
+    departmentsQuery.refetch();
+    setBackdropAndSnackbar(severity, message);
+  };
+
+  const createDepartmentMutation = trpc.department.createDepartment.useMutation(
+    {
+      onSuccess: () =>
+        refetchDepartments("success", "部署の作成が完了しました。"),
+      onError: () =>
+        setBackdropAndSnackbar("error", "部署の作成に失敗しました。"),
+    },
+  );
   const updateDepartmentMutation =
-    trpc.department.updateDepartmentById.useMutation(refetchDepartments);
+    trpc.department.updateDepartmentById.useMutation({
+      onSuccess: () =>
+        refetchDepartments("success", "部署の更新が完了しました。"),
+      onError: () =>
+        setBackdropAndSnackbar("error", "部署の更新に失敗しました。"),
+    });
   const updateDepartmentsOrderMutation =
     trpc.department.updateDepartmentsOrder.useMutation();
   const deleteDepartmentMutation =
-    trpc.department.deleteDepartmentById.useMutation(refetchDepartments);
-
-  const [departments, setDepartments] = useState<Departments>();
+    trpc.department.deleteDepartmentById.useMutation({
+      onSuccess: () =>
+        refetchDepartments("success", "部署の削除が完了しました。"),
+      onError: () =>
+        setBackdropAndSnackbar("error", "部署の更新に失敗しました。"),
+    });
 
   useEffect(() => {
     setDepartments(departmentsQuery.data);
@@ -53,6 +81,7 @@ export default function Department() {
   };
 
   const createDepartmentData = (data: DepartmentFormSchemaType) => {
+    setBackdrop(true);
     createDepartmentMutation.mutate(data);
   };
 
@@ -60,10 +89,12 @@ export default function Department() {
     departmentId: number,
     data: DepartmentFormSchemaType,
   ) => {
+    setBackdrop(true);
     updateDepartmentMutation.mutate({ id: departmentId, name: data.name });
   };
 
   const deleteDepartmentData = (departmentId: number) => {
+    setBackdrop(true);
     deleteDepartmentMutation.mutate({ id: departmentId });
   };
 
