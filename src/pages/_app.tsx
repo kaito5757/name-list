a7b13fa0ed7cfba637f965399b17bc8c013b2e65
kaito121855 <kaito.121855@gmail.com>
2024-01-20 22:15:45 +0900

    add: SpeedInsightsコンポーネントの適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { trpc } from "../utils/trpc";
 import GlobalLayout from "@/components/parts/common/GlobalLayout";
 import TransitionProgress from "@/components/parts/common/TransitionProgress";
+import "@/styles/globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import type { AppProps } from "next/app";
+import { trpc } from "../utils/trpc";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalLayout>
       <TransitionProgress />
       <Component {...pageProps} />
+      <SpeedInsights />
     </GlobalLayout>
   );
 }
